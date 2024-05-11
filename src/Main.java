f4fdd72910e431a0684f1a9e268b3185aa51d509
Ang3l1485 <maortizp@eafit.edu.co>
2024-05-10 23:37:27 -0500

    se agrega la funcion nombre en el main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Nombre();
+
+    }
+    static void Nombre(){
+        System.out.println("assigment 3 miguel anegl ortiz puerta");
     }
 }
